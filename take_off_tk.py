86d8ee868eda3faa550bdc48cfa6815378ca922e
yothebob <70663174+yothebob@users.noreply.github.com>
2021-07-26 14:02:46 -0700

    added enums, cleaned repeating code. minor refactoring.

--- take_off_tk.py
+++ take_off_tk.py
@@ -4,33 +4,69 @@ import openpyxl
 from openpyxl import Workbook
 from openpyxl.styles.borders import Border,Side
 import math
+from enum import Enum
 
-window=Tk()
-window.title('Take Off Tool 0.2')
+"""
+This was made to take in railing sections and give you an excel estimate based off the TR,BR,infill and etc you pick.
+This also has the capability to create an estimate with total LF only also using the given TR,BR,infill. This is still in
+trial and the total LF works based off statistics of previous estimates.
+
+
+
+"""
+
+class TopRail:
+    #Enum top rails
+    TR200 = "TR200"
+    TR375 = "TR375"
+    TR400 = "TR400"
+    TR670 = "TR670"
+
+class BottomRail:
+    BR200 = "BR200"
+    BR100 = "BR100"
+
+class Mount:
+    BP = "BP"
+    FASCIA = "Fasica"
+    HALFENS = "Halfen"
+    COREHOLE = "Corehole"
+
+class Infill:
+    PICKET = "Picket"
+    CABLE = "Cable"
+    GLASS = "Glass"
+
 
 def total_posts(_spacing):
+    #return total number of posts and post types per spacing, only works with input_lengths
     sections = section.get('1.0',END)
-    corners=0
-    end_posts=0
-    line_posts=0
-    total_posts=0
-    total_sections=0
+    outside_corner = 0
+    inside_corner = 0
+    end_posts = 0
+    line_posts = 0
+    total_posts = 0
+    total_sections = 0
     new_sect = sections.replace('\n',',')
     new_sect = list(new_sect.split(','))
     for line in new_sect:
-        res = re.findall(r"\d?\d?\.?\d\d?",line)
-        corners += len(re.findall(r"[x]",line))
+        res = re.findall(r"\d?\d?[.]?\d?\d?",line)
+        print(res)
+        inside_corner += len(re.findall(r"[x]",line))
+        outside_corner += len(re.findall(r"[*]",line))
         total_sections += 1
         for item in res:
-            total_posts += math.ceil(float(item)/ _spacing)
+            if item != "":
+                total_posts += math.ceil(float(item)/ _spacing)
     total_posts += total_sections
     end_posts = total_sections *2
-    corner_posts = corners
+    corner_posts = outside_corner + inside_corner
     line_posts = total_posts - corner_posts - end_posts
     return total_posts,end_posts,corner_posts,line_posts
 
 
 def return_spl200():
+    #returns the amount of SPL200 parts needed
     spl200=0
     sections = section.get('1.0',END)
     new_sect= sections.replace('\n',',')
@@ -41,10 +77,11 @@ def return_spl200():
             if float(item) > 20:
                 spl200 += math.floor(float(item)/20)
     return spl200;
-    
+
 
 
 def total_tr(_tr_len=20):
+    #totals and returns top railing, given a specific top rail length 
     sections = section.get('1.0',END)
     total_tr = 0
     total_scrap = []
@@ -57,10 +94,10 @@ def total_tr(_tr_len=20):
         for item in res:
             total_runs.append(float(item))
             total_scrap.append(0)
-    
+
     total_runs = sorted(total_runs,reverse=True)
     print(total_runs)
-    
+
     for run in range(len(total_runs)):
         print('RUN: ' + str(total_runs[run]))
         new_peice = True
@@ -79,11 +116,12 @@ def total_tr(_tr_len=20):
             if new_peice == True:
                 total_tr += 1
                 total_scrap[run] = _tr_len - total_runs[run]
-            
+
     return total_tr
 
 
 def total_lf():
+    # adds all sections together to return total LF
     lf=0
     sections = section.get('1.0',END)
     new_sect= sections.replace('\n',',')
@@ -92,11 +130,12 @@ def total_lf():
         res = re.findall('\d?\d?\.?\d?\d',line)
         for item in res:
             lf += float(item)
-    return lf
     print("Total LF: " + str(lf))
+    return lf
 
 
 def make_xlsm(part_names,part_list):
+    # make excel estimate given a list of parts and a list of part names
     wb=Workbook()
     color = openpyxl.styles.colors.Color(rgb='E5E7E6')
     my_fill = openpyxl.styles.fills.PatternFill(patternType='solid', fgColor=color)
@@ -108,15 +147,15 @@ def make_xlsm(part_names,part_list):
     sheet['B2'] = 'Part Names'
     sheet['C2'] = 'Quantity'
     sheet['D2'] = 'Field Measure'
-    
+
     for num in range(len(part_list)):
         sheet.row_dimensions[num+3].height = 20
         sheet['B' + str(num+3)] = part_names[num]
         sheet['C'+ str(num+3)] = part_list[num]
+        sheet['D' + str(num+3)].fill = my_fill
         sheet['B' + str(num+2)].border = thin_border
         sheet['C'+ str(num+2)].border = thin_border
         sheet['D'+ str(num+2)].border = thin_border
-        sheet['D' + str(num+3)].fill = my_fill
     sheet.column_dimensions['B'].width = 15
     sheet.column_dimensions['D'].width = 20
 
@@ -124,9 +163,14 @@ def make_xlsm(part_names,part_list):
     print('Excel Sheet Made!!')
 
 
-def total_parts_sc(_array):
-    
-    part_names = ['Total LF','TR','BR','Pocket infill','Flat infill','SPE1', 'SPE2','PEU','p421','p422','fmpbs1','fmpbs2','Halfen','Corner Halfen','L bracket','pt-420','PVI','spacer 100','spacer 200','rcb1','rcb2','rcb screws','pc','gvs bot','gvs top','spl200','int90','int135','end plate','EP screw','SDS bag','NC/CW','lags',' Kwikset grout']
+def total_parts_sections():
+    #total parts using lf sections 
+    part_names = ['Total LF','TR','BR','Pocket infill','Flat infill','SPE1', 'SPE2','PEU','p421','p422',
+                  'fmpbs1','fmpbs2','Halfen','Corner Halfen','L bracket','pt-420','PVI','spacer 100',
+                  'spacer 200','rcb1','rcb2','rcb screws','pc','gvs bot','gvs top','spl200','int90','int135',
+                  'end plate','EP screw','SDS bag','NC/CW','lags',' Kwikset grout']
+
+    _array = find_des()
 
     job_lf = total_lf()
     tr = total_tr()
@@ -137,7 +181,7 @@ def total_parts_sc(_array):
         total_post,end_post,corner_post,line_post = total_posts(4)
     elif _array[3] == 2:#cable
         total_post,end_post,corner_post,line_post = total_posts(3)
-    
+
     int90=0
     spl200= 0
     int90 = 0
@@ -171,7 +215,7 @@ def total_parts_sc(_array):
     stair_bp = 0
     p421 = 0
     p422 = 0
-    #----- mounting ------ 
+    #----- mounting ------
     if _array[0] == 0: #baseplate
         nccw = total_post * 4
         lags = nccw
@@ -179,25 +223,25 @@ def total_parts_sc(_array):
             p421 = total_post
         else:
             p422 = total_post
-        
+
     elif _array[0] == 1: # fascia
         fmpbs1 = total_post - corner_post
         fmpbs2 = corner_post
         peu = math.ceil(total_post/4)
         nccw = total_post * 4
         lags = nccw
-        
-    elif _array[0] == 3: # corehole
-        peu = total_post/4
-        grout = math.ceil(total_post / 12)
-    else:
+
+    elif _array[0] == 2: # Halfen
         halfen = total_post - corner_post
         corner_halfen = corner_post
         l_bracket = total_post * 2
-    #-----------infill type/ top rail type------------     
+    else:
+        peu = total_post/4
+        grout = math.ceil(total_post / 12)
+    #-----------infill type/ top rail type------------
     if _array[3] == 0:#Picket
         picket = job_lf*3
-            
+
         if _array[1] == 1:#TR375
             rcb1 += ((total_post - end_post) * 2) + (end_post)
             rcb_screw += (((total_post - end_post) * 2) + (end_post)) *2
@@ -210,7 +254,7 @@ def total_parts_sc(_array):
             sds_screw += total_post * 4
             spl200 = return_spl200()
             int90 = corner_post
-        
+
         if _array[2] == 0:#br200
             pvi = math.ceil((job_lf)/10)
             rcb2 += ((total_post - end_post) * 2) + (end_post)
@@ -223,10 +267,10 @@ def total_parts_sc(_array):
             rcb_screw += (((total_post - end_post) * 2) + (end_post)) * 2
             spacer100 = picket * 2
             sds_screw += ((total_post - end_post)*4) + (end_post *2)
-            
+
     elif _array[3] == 1:#glass
         gvs_top = math.ceil(job_lf/10)
-            
+
         if _array[1] == 1:#tr375
             rcb1 += ((total_post - end_post) * 2) + (end_post)
             rcb_screw += (((total_post - end_post) * 2) + (end_post)) *2
@@ -239,7 +283,7 @@ def total_parts_sc(_array):
             sds_screw += ((total_post - end_post)*4) + (end_post *2)
             spl200 = return_spl200()
             int90 = corner_post
-            
+
         if _array[2] ==0:#br200
             gvs_bot = gvs_top
             rcb2 += ((total_post - end_post) * 2) + (end_post)
@@ -249,39 +293,42 @@ def total_parts_sc(_array):
             rcb1 += ((total_post - end_post) * 2) + (end_post)
             rcb_screw += (((total_post - end_post) * 2) + (end_post)) *2
             sds_screw += ((total_post - end_post)*4) + (end_post *2)
- 
+
     elif _array[3] == 2:#cable
         spacing = 3
-        sds_screw += post * 4
-        
+        sds_screw += total_post * 4
+
         if _array[1] == 1:#tr375
-            rcb = post * 4
-            pc = post
+            rcb = total_post * 4
+            pc = total_post
             spe1 = tr
         else:
             end_plate = end_post
             ep_screw = end_plate *2
             flat_infill = tr
-            rcb = post * 2
+            rcb = total_post * 2
             spl200 = return_spl200()
             int90 = corner_post
-            
+
         rcb_screw = rcb * 2
         spe2 = br
 
     sds_screw = math.ceil(sds_screw /25)
-    part_list = [job_lf,tr,br, pocket_infill,flat_infill,spe1, spe2,peu,p421,p422,fmpbs1,fmpbs2,halfen,corner_halfen,l_bracket,picket,pvi,spacer100,spacer200,rcb1,rcb2,rcb_screw,pc,gvs_bot,gvs_top,spl200,int90,int135,end_plate,ep_screw,sds_screw,nccw,lags,grout]
+    part_list = [job_lf,tr,br, pocket_infill,flat_infill,spe1, spe2,peu,p421,p422,fmpbs1,fmpbs2,
+    halfen,corner_halfen,l_bracket,picket,pvi,spacer100,spacer200,rcb1,rcb2,rcb_screw,pc,gvs_bot,
+    gvs_top,spl200,int90,int135,end_plate,ep_screw,sds_screw,nccw,lags,grout]
     print("Take Off Finished!!")
     make_xlsm(part_names,part_list)
 
 
-def find_des_stat():
+def find_des():
+    #grab selected railing perameters and return an array with corrosponding numbers 
     res_array=[]
     res_array.append(str_mount.get())
     res_array.append(str_br.get())
     res_array.append(str_tr.get())
     res_array.append(str_infill.get())
-    
+
     for item in res_array:
         if item == 'BP':
             res_array[0]=0
@@ -291,13 +338,13 @@ def find_des_stat():
             res_array[0]=2
         elif item == 'Foam blockout':
             res_array[0]=3
-            
+
         elif item == 'BR200':
             res_array[1]=0
         elif item == 'BR100':
             res_array[1]=1
 
-            
+
         if item == 'TR200':
             res_array[2]=0
         elif item =='TR375':
@@ -306,7 +353,7 @@ def find_des_stat():
             res_array[2]=2
         elif item == 'TR670':
             res_array[2]=3
-            
+
         elif item == 'Picket':
             res_array[3]=0
         elif item == 'Cable':
@@ -316,59 +363,19 @@ def find_des_stat():
     return res_array
 
 
-def find_des():
-    res_array=[]
-    res_array.append(str_mount.get())
-    res_array.append(str_br.get())
-    res_array.append(str_tr.get())
-    res_array.append(str_infill.get())
-    
-    for item in res_array:
-        if item == 'BP':
-            res_array[0]=0
-        elif item == 'Fascia':
-            res_array[0]=1
-        elif item == 'Halfens':
-            res_array[0]=2
-        elif item == 'Foam blockout':
-            res_array[0]=3
-
-        elif item == 'BR200':
-            res_array[1]=0
-        elif item == 'BR100':
-            res_array[1]=1
-
-
-        if item == 'TR200':
-            res_array[2]=0
-        elif item =='TR375':
-            res_array[2]=1
-        elif item == 'TR400':
-            res_array[2]=2
-        elif item == 'TR670':
-            res_array[2]=3
-
-
-        elif item == 'Picket':
-            res_array[3]=0
-        elif item == 'Cable':
-            res_array[3]=1
-        elif item == 'Glass':
-            res_array[3]=2
-    total_parts_sc(res_array)
-
-
-
 def total_parts_stats():
-    
-    part_names = ['Total LF','TR','BR','Pocket infill','Flat infill','SPE1', 'SPE2','PEU','p421','p422','fmpbs1','fmpbs2','Halfen','Corner Halfen','L bracket','pt-420','PVI','spacer 100','spacer 200','rcb1','rcb2','rcb screws','pc','gvs bot','gvs top','spl200','int90','int135','end plate','EP screw','SDS bag','NC/CW','lags',' Kwikset grout']
+
+    part_names = ['Total LF','TR','BR','Pocket infill','Flat infill','SPE1', 'SPE2','PEU','p421',
+                  'p422','fmpbs1','fmpbs2','Halfen','Corner Halfen','L bracket','pt-420','PVI',
+                  'spacer 100','spacer 200','rcb1','rcb2','rcb screws','pc','gvs bot','gvs top',
+                  'spl200','int90','int135','end plate','EP screw','SDS bag','NC/CW','lags',' Kwikset grout']
 
     job_lf = total_lf()
     tr = math.ceil((job_lf/20)*1.15)
     br = tr
-    
-    _array= find_des_stat()
-    
+
+    _array= find_des()
+
     if _array[3] == 0:#picket
         total_post = round(889.3+(-.335-889.3)/(1+(job_lf/1656.11)))
     elif _array[3] == 2: #cable
@@ -413,7 +420,7 @@ def total_parts_stats():
     stair_bp = 0
     p421 = 0
     p422 = 0
-      
+
     if _array[0] == 0: #baseplate
         nccw = total_post * 4
         lags = nccw
@@ -421,14 +428,14 @@ def total_parts_stats():
             p421 = total_post
         else:
             p422 = total_post
-        
+
     elif _array[0] == 1: # fascia
         fmpbs1 = total_post - corner_post
         fmpbs2 = corner_post
         peu = math.ceil(total_post/4)
         nccw = total_post * 4
         lags = nccw
-        
+
     elif _array[0] == 3: # corehole
         peu = total_post/4
         grout = math.ceil(total_post / 12)
@@ -436,10 +443,10 @@ def total_parts_stats():
         halfen = total_post - corner_post
         corner_halfen = corner_post
         l_bracket = total_post * 2
-#---------------------------------------------------------     
+#---------------------------------------------------------
     if _array[3] == 0:#Picket
         picket = job_lf*3
-            
+
         if _array[1] == 1:#TR375
             rcb1 += ((total_post - end_post) * 2) + (end_post)
             rcb_screw += (((total_post - end_post) * 2) + (end_post)) *2
@@ -452,7 +459,7 @@ def total_parts_stats():
             sds_screw += total_post * 4
             spl200 = 0#return_spl200()
             int90 = corner_post
-        
+
         if _array[2] == 0:#br200
             pvi = math.ceil((job_lf)/10)
             rcb2 += ((total_post - end_post) * 2) + (end_post)
@@ -465,10 +472,10 @@ def total_parts_stats():
             rcb_screw += (((total_post - end_post) * 2) + (end_post)) * 2
             spacer100 = picket * 2
             sds_screw += ((total_post - end_post)*4) + (end_post *2)
-            
+
     elif _array[3] == 1:#glass
         gvs_top = math.ceil(job_lf/10)
-            
+
         if _array[1] == 1:#tr375
             rcb1 += ((total_post - end_post) * 2) + (end_post)
             rcb_screw += (((total_post - end_post) * 2) + (end_post)) *2
@@ -481,7 +488,7 @@ def total_parts_stats():
             sds_screw += ((total_post - end_post)*4) + (end_post *2)
             spl200 = 0#return_spl200()
             int90 = corner_post
-            
+
         if _array[2] ==0:#br200
             gvs_bot = gvs_top
             rcb2 += ((total_post - end_post) * 2) + (end_post)
@@ -492,47 +499,52 @@ def total_parts_stats():
             rcb_screw += (((total_post - end_post) * 2) + (end_post)) *2
             sds_screw += ((total_post - end_post)*4) + (end_post *2)
 
-            
+
     elif _array[3] == 2:#cable
         spacing = 3
-        sds_screw += post * 4
-        
+        sds_screw += total_post * 4
+
         if _array[1] == 1:#tr375
-            rcb = post * 4
-            pc = post
+            rcb = total_post * 4
+            pc = total_post
             spe1 = tr
         else:
             end_plate = end_post
             ep_screw = end_plate *2
             flat_infill = tr
-            rcb = post * 2
+            rcb = total_post * 2
             spl200 = return_spl200()
             int90 = corner_post
-            
+
         rcb_screw = rcb * 2
         spe2 = br
 
     sds_screw = math.ceil(sds_screw /25)
-    part_list = [job_lf,tr,br, pocket_infill,flat_infill,spe1, spe2,peu,p421,p422,fmpbs1,fmpbs2,halfen,corner_halfen,l_bracket,picket,pvi,spacer100,spacer200,rcb1,rcb2,rcb_screw,pc,gvs_bot,gvs_top,spl200,int90,int135,end_plate,ep_screw,sds_screw,nccw,lags,grout]
+    part_list = [job_lf,tr,br, pocket_infill,flat_infill,spe1, spe2,peu,p421,p422,fmpbs1,fmpbs2,
+    halfen,corner_halfen,l_bracket,picket,pvi,spacer100,spacer200,rcb1,rcb2,rcb_screw,pc,gvs_bot,
+    gvs_top,spl200,int90,int135,end_plate,ep_screw,sds_screw,nccw,lags,grout]
     print("Take Off Finished!!")
     make_xlsm(part_names,part_list)
 
 
+
+#creating gui for running 
+window=Tk()
+window.title('Take Off Tool 0.2')
 
 str_tr= StringVar(window)
 str_br= StringVar(window)
 str_mount= StringVar(window)
 str_infill= StringVar(window)
 
-str_tr.set('TR200')
-str_br.set('BR200')
-str_mount.set('BP')
-str_infill.set('Picket')
+str_tr.set(TopRail.TR200)
+str_br.set(BottomRail.BR200)
+str_mount.set(Mount.BP)
+str_infill.set(Infill.PICKET)
 
 section = Text(width=80,height=25)
 section.grid(row=1,column=0)
-enter_button = Button(text='Run (Sections ONLY)',command=find_des).grid(row=8,column=0)
-sect_label= Label(text='Sections: (10x10)').grid(row=0,column=0)
+sect_label= Label(text='Sections: (10x10) for inside corner use *').grid(row=0,column=0)
 tr_box = OptionMenu(window,str_tr,'TR200','TR375','TR400','TR670').grid(row=2,column=0)
 br_box = OptionMenu(window,str_br,'BR200','BR100').grid(row=3,column=0)
 mount_box= OptionMenu(window,str_mount,'BP','Foam Blockout','Halfen','Fascia').grid(row=4,column=0)
@@ -540,5 +552,7 @@ infill_box= OptionMenu(window,str_infill,'Picket','Glass','Cable').grid(row=5,co
 take_off_label = Label(text='Job Name:').grid(row=6, column=0)
 take_off_name = Text(width=30,height=1)
 take_off_name.grid(row=7,column=0)
+enter_section_button = Button(text='Run (Sections ONLY)',command=total_parts_sections).grid(row=8,column=0)
 enter_stat_button = Button(text="Run (LF ONLY)",command=total_parts_stats).grid(row=9,column=0)
 window.mainloop()
+
